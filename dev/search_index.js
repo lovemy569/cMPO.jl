b420898dd6767420425ac9a8f980c62838ba5c59
Documenter.jl <documenter@juliadocs.github.io>
2024-06-07 06:16:42 +0000

    build based on 2293eeb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = cMPO","category":"page"},{"location":"#cMPO","page":"Home","title":"cMPO","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for cMPO.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [cMPO]","category":"page"}]
+}
